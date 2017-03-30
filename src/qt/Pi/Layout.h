929ba24c76e9862f56e2050ccf7749e466e087b4
chuckfairy <charlieabeling@gmail.com>
2017-03-29 23:59:18 -0500

    Cleanup, Build type start pi cmake

---
+++ src/qt/Pi/Layout.h
@@ -0,0 +1,35 @@
+/**
+ * Pi layout
+ */
+#pragma once
+
+namespace Orza { Qt { Pi {
+
+/**
+ * Forwarding
+ */
+
+
+/**
+ * Main class
+ */
+
+class Layout {
+
+
+    private:
+
+        /**
+         * Fullscreen flag
+         */
+
+        bool FULLSCREEN = false;
+
+
+    public:
+
+        Layout();
+
+};
+
+} } };
