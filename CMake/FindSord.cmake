22269a4628c8756a677cd5d06c5e6f4312e818a4
chuckfairy <charlieabeling@gmail.com>
2017-09-14 15:32:48 -0500

    find sord update fix

--- CMake/FindSord.cmake
+++ CMake/FindSord.cmake
@@ -12,10 +12,12 @@ if( NOT SORD_INCLUDE_DIRS )
     set(SORD_INCLUDE_DIRS
       "${ORZABAL_ROOT_PATH}/lib-src/sord/src"
       "${ORZABAL_ROOT_PATH}/lib-src/sord"
+      "${ORZABAL_ROOT_PATH}/lib-src/sord/sord"
     )
 
     set(SORD_INCLUDE_FILES
-      "${ORZABAL_ROOT_PATH}/lib-src/sord/src/**/*.c"
+      "${ORZABAL_ROOT_PATH}/lib-src/sord/src/*.c"
+      "${ORZABAL_ROOT_PATH}/lib-src/sord/src/zix/*.c"
     )
 
     message( "${SORD_INCLUDE_DIR}" )
