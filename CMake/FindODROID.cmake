f6510d47f07570a0d8b812279f805aa0227ca309
charlieabeling@gmail.com <charlieabeling@gmail.com>
2017-10-18 18:34:55 -0400

    odroid cmake fix

--- CMake/FindODROID.cmake
+++ CMake/FindODROID.cmake
@@ -4,7 +4,7 @@ EXEC_PROGRAM( "cat /proc/cpuinfo | grep -o ODROID.*"
     OUTPUT_VARIABLE ODROID_TYPE )
 
 if( "${ODROID_TYPE}" STREQUAL "" )
-    set( IS_ODROID false )
+    set( IS_ODROID 0 )
 else()
-    set( IS_ODROID true )
+    set( IS_ODROID 1 )
 endif()
